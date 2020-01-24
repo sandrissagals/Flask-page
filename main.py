dbad51887a2b5f8131fb513911a0ae49840e9a3f
Sandris Sagals <sandris.sagals@gmail.com>
2020-01-24 06:43:17 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+  return "<a href='/home'>Hi!</a>"
+
+@app.route('/home')
+def home():
+  return render_template('home.html')
+
+@app.route('/about')
+def about():
+  return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+  # Pieslegtsanas DB
+  return render_template('contact.html', phone = 778787)
+
+if __name__ == '__main__':
+  app.run(host = '0.0.0.0', port = 5211, threaded = True, debug = True)
